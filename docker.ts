f153aae0feba1a161c19be15c9909d31419f96ff
cortez <crtzrms@gmail.com>
2025-06-18 14:50:11 -0300

    Simplify container names

--- docker.ts
+++ docker.ts
@@ -41,7 +41,8 @@ async function getContainerStats(containerId: string): Promise<any> {
 }
 
 function formatMetrics(name: string, stats: any) {
-  const labels = `{container="${name}",hostname="${HOSTNAME}"}`;
+  const parsedName = name.split('.')[0];
+  const labels = `{container="${parsedName}",hostname="${HOSTNAME}"}`;
   const cpuDelta = stats.cpu_stats.cpu_usage.total_usage - stats.precpu_stats.cpu_usage.total_usage;
   const systemDelta = stats.cpu_stats.system_cpu_usage - stats.precpu_stats.system_cpu_usage;
   const onlineCpus = stats.cpu_stats.online_cpus || 1;
